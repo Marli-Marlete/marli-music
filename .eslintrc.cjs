1813818b1d681960e499ef19c5b9d1422b4c54bc
Alan Alves <alan.alves@pinpeople.com.br>
2023-04-16 15:24:40 -0300

    feat(eslint config): adding eslint file config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,18 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: "eslint:recommended",
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    plugins: ["sort-keys-fix"],
-    rules: {
-        "sort-keys-fix/sort-keys-fix": "warn",
-    },
+	parser: '@typescript-eslint/parser',
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'sort-keys-fix'],
+	rules: {
+		'sort-keys-fix/sort-keys-fix': 'warn',
+	},
 };
