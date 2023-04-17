a87fb9d564beae0398b8d7e00ccfe06cc23cb5fe
badaueba <alan.demit@hotmail.com>
2023-04-16 21:57:39 -0300

    chore: fixing eslint file name

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,17 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	root: true,
 	env: {
 		browser: true,
 		es2021: true,
 	},
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 	overrides: [],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
 	plugins: ['@typescript-eslint', 'sort-keys-fix'],
+	root: true,
 	rules: {
 		'sort-keys-fix/sort-keys-fix': 'warn',
 	},
