78c91bed9e4c11cede31e4dc57d17c5a2ad98813
badaueba <alan.demit@hotmail.com>
2023-04-20 22:37:56 -0300

    fix(bot-hooks): adding removeAutoPause

--- src/bot/bot-hooks.ts
+++ src/bot/bot-hooks.ts
@@ -33,9 +33,9 @@ export function startBotHooks(
 	});
 
 	player.on('stateChange', (oldState, newState) => {
-		logger.log('info', `audio player changes from ${oldState.status} to ${newState.status} `,);
+		logger.log('info', `audio player changes from ${oldState.status} to ${newState.status}`,);
 		if (newState.status === AudioPlayerStatus.Paused) player.unpause();
-		if (newState.status === AudioPlayerStatus.AutoPaused) player.unpause();
+		if (newState.status === AudioPlayerStatus.AutoPaused) removeAutoPause(player)
 	});
 
 	player.on(AudioPlayerStatus.Idle, () => {
@@ -45,10 +45,22 @@ export function startBotHooks(
 
 	player.on(AudioPlayerStatus.Playing, () => {
 		logger.log('info', 'Audio Player is currently playing');
+		clearRemoveAutoPuase(player)
 	});
 
 	player.on('error', (error: Error) => {
 		logger.log('error', 'audio player error', error);
 		sentryCapture('player.error', error);
 	});
+}
+
+
+const removeAutoPause = (player: AudioPlayer) => setInterval(() => {
+	player.checkPlayable()
+	player.unpause()
+	logger.log("info", `removeAutoPause: ${player.state.status}`)
+}, 1000)
+
+const clearRemoveAutoPuase = (player:AudioPlayer) => {
+	clearInterval(removeAutoPause.bind(player));
 }
