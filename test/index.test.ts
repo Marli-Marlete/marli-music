c7898e73d27b403c1747b2da0da30a980c527697
badaueba <alan.demit@hotmail.com>
2023-04-22 02:33:14 -0300

    test(index): adding more suites

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,16 +2,22 @@ import test from 'ava';
 import { after, before, describe, it } from 'node:test';
 import Sinon from 'sinon';
 import { botStartup } from '../src/bot/';
+import { initConfigs } from '../src/config';
+import { MarliMusic } from '../src/bot/marli-music';
 
 test('index.ts', (t) => {
 	const botToken = 'TEST_TOKEN';
 	const botPrefix = '/';
-	before(() => {
-		Sinon.spy(botStartup);
-	});
+
+	before(() => {});
 
 	after(() => {
 		Sinon.restore();
+	});
+
+	it('initConfigs should run correctly', () => {
+		const fakeBotStartup = Sinon.spy(initConfigs);
+		t.deepEqual(fakeBotStartup.calledOnce, true, 'should be called onced');
 	});
 
 	describe('bot env vars should be set', () => {
@@ -20,6 +26,14 @@ test('index.ts', (t) => {
 	});
 
 	it('botStartup should run correctly', () => {
-		t.assert(botStartup, 'should exists');
+		const fakeBotStartup = Sinon.spy(botStartup);
+		t.deepEqual(fakeBotStartup.calledOnce, true, 'should be called onced');
+	});
+
+	it('botStartup should run correctly', () => {
+		const fakeBotStartup = Sinon.stub(botStartup, () => {
+			return new MarliMusic();
+		});
+		t.deepEqual(fakeBotStartup.calledOnce, true, 'should be called onced');
 	});
 });
