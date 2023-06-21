290ea271ff5f622cbb92fd401e6237ddaf7b5754
Alan Alves <alan.demit@hotmail.com>
2023-06-21 00:21:05 -0300

    fix(src/bot): adding reply message to skip command

--- src/bot/commands/command-skip.ts
+++ src/bot/commands/command-skip.ts
@@ -4,31 +4,34 @@ import { MarliMusic } from '../marli-music';
 import { Command } from './command';
 
 export class Skip extends Command {
-	constructor(bot: MarliMusic) {
-		super(bot);
-		this.name = 'skip';
-	}
-	async execute(message: Message) {
-		try {
-			await this.validate(message, 'skip');
-			const connectionID = message.member.voice.channelId;
-			const player = this.getPlayer(connectionID);
-			const queue = this.getQueue();
-			const playlist = queue.getList(connectionID);
+  constructor(bot: MarliMusic) {
+    super(bot);
+    this.name = 'skip';
+  }
+  async execute(message: Message) {
+    try {
+      await this.validate(message, 'skip');
+      const connectionID = message.member.voice.channelId;
+      const player = this.getPlayer(connectionID);
+      const queue = this.getQueue();
+      const playlist = queue.getList(connectionID);
 
-			if (playlist.length) {
-				const next = playlist[0];
-				await message.reply({
-					content: `${message.author.username} ${BOT_MESSAGES.MUSIC_SKIPPED} ${next.streamInfo.title}`,
-				});
-				player.play(next.audioResource);
-				queue.pop(connectionID);
-			} else {
-				queue.clear(connectionID);
-				player.stop();
-			}
-		} catch (error) {
-			await this.sendCommandError(error, message);
-		}
-	}
+      if (playlist.length) {
+        const next = playlist[0];
+        await message.reply({
+          content: `${message.author.username} ${BOT_MESSAGES.MUSIC_SKIPPED} ${next.streamInfo.title}`,
+        });
+        player.play(next.audioResource);
+        queue.pop(connectionID);
+      } else {
+        await message.reply({
+          content: `${message.author.username} ${BOT_MESSAGES.MUSIC_STOPPED}`,
+        });
+        queue.clear(connectionID);
+        player.stop();
+      }
+    } catch (error) {
+      await this.sendCommandError(error, message);
+    }
+  }
 }
