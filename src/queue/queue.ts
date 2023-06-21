4d7f92e96f445f4fa99decf4c2b5fdf36339464e
Alan Alves <alan.demit@hotmail.com>
2023-06-21 00:22:16 -0300

    fix(src/queue): avoiding undefined when we have no item

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -3,51 +3,51 @@ import { StreamInfo } from 'sources/source-stream';
 import { AudioResource } from '@discordjs/voice';
 
 export interface QueueData {
-	streamInfo: StreamInfo;
-	audioResource: AudioResource;
+  streamInfo: StreamInfo;
+  audioResource: AudioResource;
 }
 
 export abstract class Queue {
-	items: Map<string, QueueData[]> = new Map();
+  items: Map<string, QueueData[]> = new Map();
 
-	abstract getList(connection: string): QueueData[];
+  abstract getList(connection: string): QueueData[];
 
-	abstract add(connection: string, value: QueueData): void;
+  abstract add(connection: string, value: QueueData): void;
 
-	abstract pop(connection: string): void;
+  abstract pop(connection: string): void;
 
-	abstract clear(connection: string): void;
+  abstract clear(connection: string): void;
 }
 
 export class LocalQueue extends Queue {
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	getList(connectionID: string): QueueData[] {
-		return this.items.get(connectionID);
-	}
+  getList(connectionID: string): QueueData[] {
+    return this.items.get(connectionID) || [];
+  }
 
-	add(connectionID: string, value: QueueData) {
-		const connectionItems = this.getList(connectionID);
+  add(connectionID: string, value: QueueData) {
+    const connectionItems = this.getList(connectionID);
 
-		if (!connectionItems) {
-			this.items.set(connectionID, [value]);
-		} else {
-			connectionItems.push(value);
-		}
-	}
+    if (!connectionItems) {
+      this.items.set(connectionID, [value]);
+    } else {
+      connectionItems.push(value);
+    }
+  }
 
-	pop(connectionID: string) {
-		const connectionItems = this.getList(connectionID);
-		connectionItems.shift();
-		this.items.set(connectionID, connectionItems);
-	}
+  pop(connectionID: string) {
+    const connectionItems = this.getList(connectionID);
+    connectionItems.shift();
+    this.items.set(connectionID, connectionItems);
+  }
 
-	clear(connectionID: string): void {
-		const connectionItems = this.getList(connectionID);
-		if (connectionItems) {
-			this.items.set(connectionID, []);
-		}
-	}
+  clear(connectionID: string): void {
+    const connectionItems = this.getList(connectionID);
+    if (connectionItems) {
+      this.items.set(connectionID, []);
+    }
+  }
 }
