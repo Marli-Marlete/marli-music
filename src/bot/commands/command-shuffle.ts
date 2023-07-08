134db923cb909a0ca4c95adf9233fbfacb136e9d
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-07-08 12:31:23 -0300

    feat: implement shuffle in list queue

---
+++ src/bot/commands/command-shuffle.ts
@@ -0,0 +1,22 @@
+import { Message } from 'discord.js';
+import { Command } from './command';
+import { MarliMusic } from 'bot/marli-music';
+import { ListQueue } from './command-list-queue';
+
+export class Shuffle extends Command {
+  constructor(bot: MarliMusic) {
+    super(bot);
+    this.name = 'shuffle';
+  }
+
+  async execute(message: Message<boolean>, input: string): Promise<void> {
+    try {
+
+      this.getQueue().shuffle(message.member.voice.channelId);
+
+      new ListQueue(this.bot).execute(message);
+    } catch (error) {
+      await this.sendCommandError(error, message);
+    }
+  }
+}
