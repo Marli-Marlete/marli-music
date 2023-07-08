72370cbd5bd3c24635d32b1732cf98988a75aba8
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-07-08 12:31:04 -0300

    refactor: add imports from index instead from its own files

--- src/bot/commands/command-map.ts
+++ src/bot/commands/command-map.ts
@@ -1,20 +1,14 @@
-import { CommandHelp } from './command-help';
-import { ListQueue } from './command-list-queue';
-import { Pause } from './command-pause';
-import { Play } from './command-play';
-import { Resume } from './command-resume';
-import { Search } from './command-search';
-import { Skip } from './command-skip';
-import { Stop } from './command-stop';
+import * as COMMANDS from './index';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const ALL_COMMANDS: Record<string, any> = {
-  search: Search,
-  play: Play,
-  pause: Pause,
-  resume: Resume,
-  skip: Skip,
-  stop: Stop,
-  queue: ListQueue,
-  help: CommandHelp,
+  search: COMMANDS.Search,
+  play: COMMANDS.Play,
+  pause: COMMANDS.Pause,
+  resume: COMMANDS.Resume,
+  skip: COMMANDS.Skip,
+  stop: COMMANDS.Stop,
+  queue: COMMANDS.ListQueue,
+  help: COMMANDS.CommandHelp,
+  shuffle: COMMANDS.Shuffle,
 };
