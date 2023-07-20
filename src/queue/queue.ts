0abfa39475d8594f37cf74d2c2b5ab5c5ad7f7a8
badaueba <alan.demit@hotmail.com>
2023-07-20 00:53:29 -0300

    refactor(src/bot): refactoring play and skip - adding current playing music to queue

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -5,6 +5,7 @@ import { shuffleArray } from '../helpers/helpers';
 export interface QueueData {
   streamInfo: StreamInfo;
   audioResource: AudioResource;
+  userSearch: string;
 }
 
 export abstract class Queue {
