a05af5b3687b05853a20cc9175d4f72c6788dd5d
Willian Silva <willian@wiki4fit.com.br>
2023-08-01 11:21:58 -0300

    feat: soundcloud play strategy

--- src/sources/play-dl-source/auth.ts
+++ src/sources/play-dl-source/auth.ts
@@ -3,6 +3,7 @@ import play from 'play-dl';
 const STREAM_TYPES = {
   youtube: ['yt_video', 'yt_playlist'],
   spotify: ['sp_track', 'sp_playlist', 'sp_album'],
+  soundcloud: ['so_playlist', 'so_track'],
 };
 
 function isValidStreamType(streamType: string) {
@@ -15,9 +16,25 @@ function isValidSpotifyStreamType(streamType: string) {
   return STREAM_TYPES.spotify.includes(streamType);
 }
 
+function isValidSoundCloudStreamType(streamType: string) {
+  return STREAM_TYPES.soundcloud.includes(streamType);
+}
+
 async function refreshAuthToken(streamType: string) {
   if (isValidSpotifyStreamType(streamType) && play.is_expired()) {
     await play.refreshToken();
+
+    return;
+  }
+
+  if (isValidSoundCloudStreamType) {
+    const clientId = await play.getFreeClientID();
+
+    play.setToken({
+      soundcloud: {
+        client_id: clientId,
+      },
+    });
   }
 }
 
---
+++ src/sources/play-dl-source/strategies/soundcloud-strategy.ts
@@ -0,0 +1,49 @@
+import play, { SoundCloudPlaylist, SoundCloudTrack } from 'play-dl';
+
+import { StreamInfo } from '@/sources/source-stream';
+
+import { PlayDlSourceStream } from '../play-dl-source';
+import { IStrategy } from './strategy';
+
+export class SoundCloudTrackStrategy implements IStrategy {
+  constructor(private playDlSourceStream: PlayDlSourceStream) {}
+
+  async getStreamInfo(url: string): Promise<StreamInfo[]> {
+    const soundCloudInfo = (await play.soundcloud(
+      url.trim()
+    )) as SoundCloudTrack;
+
+    const searched = (
+      await this.playDlSourceStream.search(
+        `${soundCloudInfo.name} - ${soundCloudInfo.user.name}`,
+        {
+          limit: 1,
+        }
+      )
+    ).shift();
+
+    return [
+      {
+        title: soundCloudInfo.name,
+        url: searched.url,
+        artist: soundCloudInfo.user.name,
+      },
+    ];
+  }
+}
+
+export class SoundCloudPlaylistStrategy implements IStrategy {
+  async getStreamInfo(url: string): Promise<StreamInfo[]> {
+    const playlist = (await play.soundcloud(url.trim())) as SoundCloudPlaylist;
+
+    const tracks = await playlist.all_tracks();
+
+    const soundcloudTracks = tracks.map((track) => ({
+      title: track.name,
+      url: undefined,
+      artist: track?.user.name,
+    }));
+
+    return soundcloudTracks;
+  }
+}
